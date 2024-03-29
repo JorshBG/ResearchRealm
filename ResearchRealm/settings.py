c6aff15f3b4ea26b056db83b1f7f4b7807f5bc39
JorshBG <jordiyair29@gmail.com>
2024-03-29 02:43:09 -0600

    Config embedded for localhost

--- ResearchRealm/settings.py
+++ ResearchRealm/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -110,6 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
+
+X_FRAME_OPTIONS = 'ALLOWALL'
+
 
 USE_I18N = True
 
