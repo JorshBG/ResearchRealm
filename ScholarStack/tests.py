a17cfe0f9f79aa5a6a787343938b4c9a8c011afb
JorshBG <jordiyair29@gmail.com>
2024-03-29 02:42:11 -0600

    i don't know

--- ScholarStack/tests.py
+++ ScholarStack/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
+from ScholarStack.models import Thesis
+from django.contrib.auth.models import User
+
+
 # Create your tests here.
+class TestScholarStack(TestCase):
+
+    def setUp(self):
+        file = open("testfile.txt", "w")
+        user = User.objects.filter(username="jordi").first()
+        thesis = Thesis.objects.create(
+            name='Test Thesis',
+            description='',
+            author_id=user.pk,
+            last_ip="127.0.0.1",
+            url="https://test.com",
+            content=file
+        )
